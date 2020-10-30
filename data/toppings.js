5300362b940c10577e5b4117da64960d81de8ee9
shahab <jamshidi.shahab@gmail.com>
2020-10-30 13:57:40 +1100

    Creaeted RDS and DynamoDb in the code

--- data/toppings.js
+++ data/toppings.js
@@ -1,12 +1,11 @@
 const Topping = require('../models/topping')
 const { sortBy, values } = require('lodash')
-
-const toppings = {}
+const DynamoStore = require('./dynamoStore')
 
 function init () {
-  create('Dough Crust', 'dough.png', 'dough_crust.png', 1)
-  create('Marinara Sauce', 'marinara.png', 'marinara_sauce.png', 2)
-  create('Mozzarella Cheese', 'mozzarella.png', 'mozzarella_cheese.png', 3)
+  create('Dough Crust', 'dough_crust.png', 'dough_crust.png', 1)
+  create('Marinara Sauce', 'marinara_sauce.png', 'marinara_sauce.png', 2)
+  create('Mozzarella Cheese', 'mozzarella_cheese.png', 'mozzarella_cheese.png', 3)
   create('Cheddar Cheese', 'cheddar.png', 'cheddar_cheese.png', 4)
   create('Mushrooms', 'mushroom.png', 'mushrooms.png', 5)
   create('Pepperoni', 'pepperoni.png', 'pepperonis.png', 6)
@@ -19,15 +18,15 @@ function init () {
 }
 
 async function getAll () {
-  const tops = values(toppings)
-  return sortBy(tops, ['order'])
+  const toppings = await DynamoStore.getAllItems('toppings')
+  return sortBy(toppings, ['order'])
 }
 
 async function create (name, previewImage, image, order) {
   const id = name.replace(/ /g, '_').toLowerCase()
   const topping = new Topping(id, name, previewImage, image, order)
 
-  toppings[id] = topping
+  DynamoStore.putItem('toppings', topping)
 }
 
 module.exports = {
